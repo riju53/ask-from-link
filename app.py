be4a4fbece931f91d76f1237a075e941929fd008
Tathagata Nath <nath.riju53@gmail.com>
2026-04-07 21:33:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,8 @@ import yfinance as yf
 # Load environment
 # ----------------------
 load_dotenv()
-
+os.environ["GROQ_API_KEY"] = "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"
+os.environ["HUGGINGFACEHUB_API_TOKEN"]="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"
 # ----------------------
 # Streamlit UI
 # ----------------------
